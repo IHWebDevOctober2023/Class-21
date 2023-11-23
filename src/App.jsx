2b63a2c4c7b2b12f4855747383ad5e358906efb0
marshall-bits <kz_@hotmail.es>
2023-11-23 13:39:45 +0100

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 
 import './App.css'
 import Fetching from './components/Fetching'
+import { Route, Routes, Link } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
 
 
   return (
     <>
-      <Fetching />
+      <nav>
+
+        <Link to="/" >Home</Link>
+        <span> || </span>
+        <Link to="/apartments" >Apartments</Link>
+
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/apartments" element={<Fetching />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </>
   )
 }
